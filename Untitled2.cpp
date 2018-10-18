ab506726a7eb512a555431d430adc4eecbc9c92f
sdasdas <44250248+monjoker@users.noreply.github.com>
2018-10-18 14:23:39 +0700

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout<<"*   *\t    *    \t*   *\n";
+cout<<"**  *\t   * *   \t** **\n";
+cout<<"* * *\t  *   *  \t* * *\n";
+cout<<"*  **\t ******* \t*   *\n";
+cout<<"*   *\t*       *\t*   *\n";
+
+}
